6c7c9241e905d9a239761cfd337f4b9103f22424
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-09-05 15:57:03 -0300

    feat(auth): add modules auth

---
+++ app/modules/auth/service.py
@@ -0,0 +1,151 @@
+from datetime import datetime, timedelta, timezone
+from math import floor
+from typing import Annotated
+from uuid import UUID
+
+import jwt
+from config import config
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from fastapi_async_sqlalchemy import db
+from modules.auth.model import UserSession
+from modules.auth.schema import TokenData
+from modules.users.service import get_user_by_email, get_user_by_id
+from passlib.context import CryptContext
+
+SECRET_KEY: str = config["JWT_TOKEN_SECRET"]
+ALGORITHM: str = config["ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(config["ACCESS_TOKEN_EXPIRE_MINUTES"])
+REFRESH_TOKEN_EXPIRE_MINUTES: int = int(config["REFRESH_TOKEN_EXPIRE_MINUTES"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    # TODO: Add salt
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+        )
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt, floor(expire.timestamp())
+
+
+async def authenticate_user(email: str, password: str):
+    user = await get_user_by_email(email)
+
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+
+    return user
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        user_id: str = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+
+        token_data = TokenData(user_id=user_id)
+    except:
+        raise credentials_exception
+
+    user = await get_user_by_id(token_data.user_id)
+    if user is None:
+        raise credentials_exception
+
+    if user.active is False:
+        raise HTTPException(status_code=400, detail="Inactive user")
+
+    return user
+
+
+async def create_refresh_token(user_id: str, expires_delta: timedelta | None = None):
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=REFRESH_TOKEN_EXPIRE_MINUTES
+        )
+
+    new_session = UserSession(user_id=UUID(user_id), expires_at=expire)
+
+    db.session.add(new_session)
+    await db.session.commit()
+    await db.session.refresh(new_session)
+
+    to_encode = {"sub": user_id, "jti": str(new_session.id)}
+
+    return create_access_token(
+        data=to_encode,
+        expires_delta=(
+            expires_delta
+            if expires_delta
+            else timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+        ),
+    )
+
+
+async def consume_refresh_token(refresh_token: str):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        refresh_token_data = jwt.decode(
+            refresh_token, SECRET_KEY, algorithms=[ALGORITHM]
+        )
+        if not refresh_token_data["jti"] or not refresh_token_data["sub"]:
+            raise credentials_exception
+
+        session = await db.session.get(UserSession, refresh_token_data["jti"])
+
+        if session is None:
+            raise credentials_exception
+
+        if session.expires_at < datetime.now(timezone.utc):
+            raise credentials_exception
+
+        access_token, expire = create_access_token(
+            data={"sub": refresh_token_data["sub"]}
+        )
+
+        await db.session.delete(session)
+        await db.session.commit()
+
+        refresh_token, _ = await create_refresh_token(user_id=str(session.user_id))
+
+        return {
+            "access_token": access_token,
+            "expire": expire,
+            "refresh_token": refresh_token,
+        }
+    except Exception as e:
+        raise credentials_exception
--- app/modules/users/service.py
+++ app/modules/users/service.py
@@ -1,4 +1,7 @@
+import uuid
 from sqlalchemy.orm import Session
+from fastapi_async_sqlalchemy import db
+from sqlmodel import select
 from . import model, schema
 
 
@@ -12,3 +15,13 @@ def create_user(db: Session, user: schema.UserCreate):
 
 def list_users(db: Session):
     return db.query(model.User).all()
+
+async def get_user_by_id(id: uuid.UUID | str | None):
+    return await db.session.get(model.User, id)
+
+async def get_user_by_email(email) -> model.User | None:
+    result = (await db.session.execute(select(model.User).where(model.User.email == email))).first()
+
+    return result[0] if result is not None else None
+
+
