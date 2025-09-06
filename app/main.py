33e48157419c26057c5520d7f630460eab257942
Daniel Ribas <danielribas1105@gmail.com>
2025-09-06 10:35:51 -0300

    feat(update): add readme and test connection with front

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+import datetime
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 from app.db.database import engine
 from .routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ou ["*"] durante dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 def on_startup():
@@ -13,3 +23,10 @@ def on_startup():
 @app.get("/")
 def home():
    return {"project": "GestObra"}
+
+@app.get("/status")
+def get_status():
+    return {
+        "status": "online",
+        "timestamp": datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    }
